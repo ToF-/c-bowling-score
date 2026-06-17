f6f8dd01183cddc3c4a02eb9e93288ad6e537921
ToF- <cthibauttof@gmail.com>
2026-06-17 09:57:04 +0200

    refactoring

--- src/bowling.c
+++ src/bowling.c
@@ -12,26 +12,24 @@ void init_game(struct game *game, int *rolls, int nb_rolls) {
 }
 
 int score(struct game *game) {
+
+#define NEW_FRAME(h) ( h % 2 == 0 )
+#define STRIKE(i) ( game->rolls[i] == 10 )
+#define SPARE(i) ( game->rolls[i] + game->rolls[i+1] == 10 )
+
     int result = 0;
-    int h = 0;
-    for(int i = 0; i < game->nb_rolls; i++) {
-        if(h == 20) {
-            break;
+    int half_frames = 0;
+    for(int index = 0; index < game->nb_rolls && half_frames < 20; index++) {
+        if(NEW_FRAME(half_frames) && STRIKE(index)) {
+            result += game->rolls[index+1];
+            result += game->rolls[index+2];
+            half_frames++;
         }
-        if(h % 2 == 0 && game->rolls[i] == 10) {
-            result += game->rolls[i+1];
-            result += game->rolls[i+2];
-            h += 1;
+        else if(NEW_FRAME(half_frames) && SPARE(index)) {
+            result += game->rolls[index+2];
         }
-        else if(h % 2 == 0 && game->rolls[i] + game->rolls[i+1] == 10) {
-            result += game->rolls[i+2];
-        }
-        result += game->rolls[i];
-        h += 1;
+        result += game->rolls[index];
+        half_frames++;
     }
     return result;
-}
-
-int doit() {
-    return 42;
 }
